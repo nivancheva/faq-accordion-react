d23c6523cbe9216e14a1b615a414a6ca92748fad
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-12-07 09:39:39 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,19 @@ import Question from './Question';
 const questions = [
   {
     question: 'What is Frontend Mentor, and how will it help me?',
-    answer: 'Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It is suitable for all levels and ideal for portfolio building.',
-    open: true
+    answer: 'Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It is suitable for all levels and ideal for portfolio building.'
   },
   {
     question: 'Is Frontend Mentor free?',
-    answer: 'Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.',
-    open: false
+    answer: 'Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.'
   },
   {
     question: 'Can I use Frontend Mentor projects in my portfolio?',
-    answer: 'Yes, you can use projects completed on Frontend Mentor in your portfolio. It is an excellentway to showcase your skills to potential employers!',
-    open: false
+    answer: 'Yes, you can use projects completed on Frontend Mentor in your portfolio. It is an excellentway to showcase your skills to potential employers!'
   },
   {
     question: 'How can I get help if I am stuck on a Frontend Mentor challenge?',
-    answer: 'The best place to get help is inside Frontend Mentor-s Discord community. There is a help channel where you can ask questions and seek support from other community members.',
-    open: false
+    answer: 'The best place to get help is inside Frontend Mentor-s Discord community. There is a help channel where you can ask questions and seek support from other community members.'
   }
 ]
 
